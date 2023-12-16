9da212e9733d2d058787da3f6be9dc7db2fc3062
Claes Persson <c@cp.se>
2023-12-16 15:15:05 +0100

    Adding basic structure, attempts with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Config {
+    server_type: String
+}
+
+fn generate_data() {
+}
+
+fn write_to_file() {
+}
+
+fn parse_args() {
+    let args = Config::parse();
+
+    println!("server_type: {:?}", args.server_type)
+}
+
+fn read_definition() {
+}
+
+fn send_to_remote() {
+}
+
 fn main() {
-    println!("Hello, world!");
+    parse_args();
+    read_definition();
+    generate_data();
+    write_to_file();
+    send_to_remote();
 }
