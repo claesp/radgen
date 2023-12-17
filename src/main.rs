92ca0866f353e3d90b33fdd59b2b6cbcb22ca8f4
Claes Persson <c@cp.se>
2023-12-17 11:44:16 +0100

    Restructuring config loading.

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,46 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Config {
-    server_type: String
+    server_type: String,
 }
 
-fn generate_data() {
+fn generate_data() {}
+
+fn load_config_file(global_config: &mut Config) {
+    let file_args = Config {
+        server_type: String::from("unknown"),
+    };
+
+    if file_args.server_type != global_config.server_type {
+        global_config.server_type = file_args.server_type;
+    }
 }
 
-fn load_config_file() {
+fn load_config_defaults() -> Config {
+    Config {
+        server_type: String::from("unknown"),
+    }
 }
 
-fn load_defaults() {
-}
-
-fn parse_args() -> Config {
+fn load_config_args(global_config: &mut Config) {
     let args = Config::parse();
 
-    return args;
+    if args.server_type != global_config.server_type {
+        global_config.server_type = args.server_type;
+    }
 }
 
-fn read_definition() {
-}
+fn read_definition() {}
 
-fn send_to_remote() {
-}
+fn send_to_remote() {}
 
-fn write_to_file() {
-}
+fn write_to_file() {}
 
 fn main() {
-    load_defaults();
-    load_config_file();
-
-    let args = parse_args();
-    println!("server_type: {:?}", args.server_type);
+    let mut config = load_config_defaults();
+    load_config_file(&mut config);
+    load_config_args(&mut config);
+    println!("server_type: {:?}", config.server_type);
 
     read_definition();
     generate_data();
